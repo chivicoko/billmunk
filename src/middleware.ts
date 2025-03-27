ccfc7a01f4ea82d82a5aa434ee96ec4814d548db
chivicoko <victor.c.okoye@gmail.com>
2025-03-27 02:43:39 +0100

    update: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  // const token = req.cookies.has('accessToken') ? req.cookies.get('accessToken')?.value : null;
+  const token = req.cookies.has('accessToken') ? req.cookies.get('accessToken')?.value : null;
   const role = req.cookies.has('role') ? req.cookies.get('role')?.value : null;
 
-//   if (!token) {
-//     return NextResponse.redirect(new URL('/login', req.nextUrl.origin));
-//   }
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl.origin));
+  }
 
   if (req.nextUrl.pathname.startsWith('/admin') && role !== 'ADMIN') {
     return NextResponse.redirect(new URL('/unauthorized', req.nextUrl.origin));
@@ -17,6 +17,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-//   matcher: ['/', '/dashboard', '/admin/:path*'],
-  matcher: ['/dashboard', '/admin/:path*'],
+  matcher: ['/', '/dashboard', '/admin/:path*'],
 };
